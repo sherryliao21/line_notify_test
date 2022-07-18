725c901985f2baf31dfff132a394ec53931fa5f4
Sherry Liao <sherry.liao@pioneerm.com>
2022-07-18 09:42:42 +0800

    add authenticate api call

--- fetch.js
+++ fetch.js
@@ -1,17 +1,27 @@
 const axios = require('axios')
 const dotenv = require('dotenv')
 if (process.env.ENV !== 'production') dotenv.config()
-const ngrokUrl = process.env.REDIRECT_URL   
+const redirectURL = process.env.REDIRECT_URI   
 const clientId = process.env.CLIENT_ID
-const clientSecret = process.env.CLIENT_SECRET
-// const code = process.env.CODE
-// const accessToken = process.env.ACCESS_TOKEN
+// const clientSecret = process.env.CLIENT_SECRET
+
+function authenticate() {
+  const codeForPreventingCSRF = 'f094a459-1d16-42d6-a709-c2b61ec53d60'  // 之後可看要不要用 session ID hash
+  axios({
+    method: 'get',
+    url: `https://notify-bot.line.me/oauth/authorize?response_type=code&scope=notify&response_mode=form_post&client_id=${clientId}&redirect_uri=${redirectURL}&state=${codeForPreventingCSRF}`
+  }).then(() => {
+    console.log('Authenticated!')
+  }).catch(err => {
+    console.log(err)
+  })
+}
 
 // 讓客戶敲 OAuth，會 post 我們 server，返回 code 之後組成以下這支去要 access token
-function getAccessToken(ngrokUrl, clientId, clientSecret, code) {
+function getAccessToken(redirectURL, clientId, clientSecret, code) {
   const token = axios({
     method: 'post',
-    url: `https://notify-bot.line.me/oauth/token?grant_type=authorization_code&redirect_uri=${ngrokUrl}&client_id=${clientId}&client_secret=${clientSecret}&code=${code}`,
+    url: `https://notify-bot.line.me/oauth/token?grant_type=authorization_code&redirect_uri=${redirectURL}&client_id=${clientId}&client_secret=${clientSecret}&code=${code}`,
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded'
     }
@@ -49,13 +59,10 @@ const stickers = [
 ]
 
 function sendMsg() {
-  console.log('================process.env.accesstoken: ', process.env.ACCESS_TOKEN)
-
   const accessToken = encodeURI(process.env.ACCESS_TOKEN)
-  console.log('================accessToken: ', accessToken)
   const message = ` \n機台 ${Math.floor(Math.random() * 100)} 掛掉ㄌ \n請查看`
-  const stickerPackageId = 6370
-  const stickerId = stickers[Math.floor(Math.random() * 23)]
+  // const stickerPackageId = 6370
+  // const stickerId = stickers[Math.floor(Math.random() * 23)]
   axios({
     method: 'post',
     url: encodeURI(`https://notify-api.line.me/api/notify?message=${message}`),
@@ -74,5 +81,6 @@ function sendMsg() {
 
 module.exports = {
   sendMsg,
-  getAccessToken
+  getAccessToken,
+  authenticate
 }
--- index.js
+++ index.js
@@ -2,17 +2,21 @@ const service = require('./fetch')
 const express = require('express')
 const app = express()
 const PORT = 3000
-const ngrokUrl = process.env.REDIRECT_URL   
+const redirectURL = process.env.REDIRECT_URI   
 const clientId = process.env.CLIENT_ID
 const clientSecret = process.env.CLIENT_SECRET
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+app.get('/authorize-line-notify', async (req, res) => {
+  await service.authenticate()
+  return res.status(200).send('OK')
+})
+
 app.post('/test', async (req, res) => {
   const code = req.body.code
-  const accessToken = await service.getAccessToken(ngrokUrl, clientId, clientSecret, code)
-  console.log('HERE IS THE ACCESS TOKEN: ', accessToken)
+  const accessToken = await service.getAccessToken(redirectURL, clientId, clientSecret, code)
   process.env.ACCESS_TOKEN = accessToken
   process.env.IS_AUTHORIZED = 'true'
 
